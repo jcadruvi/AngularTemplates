c72a37a4e1f0796810aba201996afa94d6209fff
Josh Cadruvi <jcadruvi@mobitor.com>
2014-01-11 19:30:48 -0800

    Adding then directive.js.

---
+++ directive.js
@@ -0,0 +1,18 @@
+(function() {
+    'use strict';
+
+    angular.module('app').directive('', );
+    
+    function () {
+        var self = {};
+
+        self.restrict = 'A';
+        self.scope = {
+        };
+
+        self.link = function (scope, element, attrs) {
+		}
+		
+		return self;
+	}
+})();
