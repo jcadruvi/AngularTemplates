867db7bf8db661b0c18cdd6015e65ebf93b44dac
Josh Cadruvi <jcadruvi@imatchative.com>
2014-07-22 07:08:23 -0700

    Added the service template.

---
+++ Templates/Scripts/service.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app').factory('$itemname$', ['Repository', $itemname$]);
+
+    function $itemname$(Repository) {
+        var self = {};
+
+
+        return self;
+    }
+})();
